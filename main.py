292bfd61471be85971bc50b3db2cb876cd961b29
Chun Shen <MY_NAME@example.comkchunshen@hotmail.com>
2021-08-07 21:39:02 +0800

    Script containing function to scrape

---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+import requests
+def shopee(item:str,by:str):
+    if by not in ['Sold','Total Sold','Price','Rating','Number 5 Star Ratings','Total Number of Ratings']:
+        print('Please spell correctly as either Sold, Total Sold, Price,Rating, Number 5 Star Ratings, Total Number of Ratings')
+        return
+    name, sold, historical_sold, price, price_max, rating, rating_5, rating_4, total_number_rating,official, brand, link=[],[],[],[],[],[],[],[],[],[],[],[]
+    shopee_url = 'https://shopee.sg/'
+    keyword_search = item
+    headers = {
+    'User-Agent': 'Chrome',
+    'Referer': '{}search?keyword={}'.format(shopee_url, keyword_search)
+    }
+    #URL to display base on keyword search and limit to 100 listings
+    url = f'{shopee_url}api/v2/search_items/?by=relevancy&keyword={keyword_search}&limit=100&newest=0&order=desc&page_type=search'
+
+    # Shopee API request
+    r = requests.get(url,headers=headers).json()
+    for i in r['items']:
+        name.append(i['name'])
+        sold.append(i['sold'])
+        historical_sold.append(i['historical_sold'])
+        price.append(i['price']/100000)
+        price_max.append(i['price_max']/100000)
+        rating.append(i['item_rating']['rating_star'])
+        rating_5.append(i['item_rating']['rating_count'][5])
+        rating_4.append(i['item_rating']['rating_count'][4])
+        total_number_rating.append(i['item_rating']['rating_count'][0])
+        official.append(i['is_official_shop'])
+        brand.append(i['brand'])
+        shop=i['shopid']
+        item=i['itemid']
+        # print(f'{shopee_url}product-i.{i['shopid']}')
+        link.append(f'{shopee_url}product-i.{shop}.{item}')
+    df = pd.DataFrame(name, columns=['Name'])
+    df['Sold'] = sold
+    df['Total Sold'] = historical_sold
+    df['Price'] = price
+    df['Price_max'] = price_max
+    df['Rating']=rating
+    df['Number 5 Star Ratings']=rating_5
+    df['Number 4 Star Ratings']=rating_4
+    df['Total Number of Ratings']=total_number_rating
+    df['Official Shop']=official
+    df['Brand']=brand
+    df['Link']=link
+    df=df.sort_values(by,ascending=False)
+    df.to_excel('Shopee_products.xlsx',index=False)
+    print('Success! :)')
+    return
+
